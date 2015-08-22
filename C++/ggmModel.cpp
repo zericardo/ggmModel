99afe39ec2888acaf7973d69471d4286f30b9fc4
Thiago Mosqueiro <thiago.mosqueiro@gmail.com>
2015-08-22 08:47:54 -0700

    Adding initial Barabasi-Albert implementation

--- C++/ggmModel.cpp
+++ C++/ggmModel.cpp
@@ -1,3 +1,13 @@
+/*
+  
+  Issues to be fixed:
+  - Still need to check for double-connections before connecting
+  - Implement update & sort CumDg
+  - Start using HashNds
+  
+ */
+
+
 #include <iostream>
 #include <random>
 #include <fstream>
@@ -7,18 +17,21 @@
 
 int main(int argc, char *argv[]){
   
-  int SEED = atoi(argv[1]);  // Seed
+  int SEED = atoi(argv[1]);  // Seed for default_random_engine
   int N = atoi(argv[2]);     // Number of nodes
   int M = atoi(argv[3]);     // Number of nodes
-  double *V;
-  V = (double *) malloc(N*sizeof(double));
-  
   
   int m0 = 5;                // Initial number of nodes
   double alpha = 1.;         // Switching probability
+  int NConns;                // Number of connections
+  
+  
+  double *CumDg, *HashNds;
+  CumDg   = (double *) malloc(N*sizeof(double));  // Cumulative sum
+  HashNds = (double *) malloc(N*sizeof(double));  // Cumulative sum
   
   // Random generators
-  std::default_random_engine generator;
+  std::default_random_engine generator(SEED);
   std::uniform_real_distribution<double> ureal(0., 1.);
   
   // Connections
@@ -26,45 +39,80 @@ int main(int argc, char *argv[]){
   
   // Index variables
   int i, j, k;
+  int from, to;
   
   
+  // ****
+  // Constructing the initial core
+  //
   
-  // Connecting the initial core
   for ( j = 0; j < m0; j++ )
   {
+    // Connecting the first m0 nodes
     for ( i = j+1; i < m0; i++ )
     {
       edge_list[i].push_back(j);
       edge_list[j].push_back(i);
     }
+    
+    // Initializing the cumulative vector
+    CumDg[j] = m0-1*(j+1);
   }
+  NConns = m0*(m0-1)/2;
   
   
+  // ****
   // Growing the network
-  for ( j = m0; j < N; j++)
+  //
+  
+  for ( from = m0; from < N; from++)
   {
     
     if ( ureal(generator) <= alpha) 
     {
       
       // Erdos-Renyi branch
-      
       for ( k = 0; k < M; k++ )
       {
 	// Choosing a pair
-	i = (j-1)*ureal(generator);
-	edge_list[i].push_back(j);
-	edge_list[j].push_back(i);
+	to = (from - 1)*ureal(generator);
+	
+	edge_list[to].push_back(from);
+	CumDg[to]++;
+		    
+	edge_list[from].push_back(to);
+	CumDg[from]++;
       }
       
     }
     else {
       
       // Barabasi-Albert branch
-      
-      
+      for ( k = 0; k < M; k++ )
+      {
+	
+	int Q = NConns*(ureal(generator));
+	i = 0;
+	while( i >= 0 )
+	  {
+	    if ( Q <= CumDg[i] )
+	      {
+		to = i; // to = HashNds[i]
+		i = -1;
+	      }
+	  }
+	
+	edge_list[from].push_back(to);
+	CumDg[from]++;
+	
+	edge_list[to].push_back(from);
+	CumDg[to]++;
+      }
       
     }
+    
+    // Updating number of connections
+    NConns += M;
   }
   
   
