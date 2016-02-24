45aa79cf8b47c853c1cb722b23630e4e22629160
José Ricardo <zericardo182@gmail.com>
2016-02-23 22:17:21 -0300

    A first implementation for the BA model part. Used the vertex bag strategy.

--- C++/ggmModel.cpp
+++ C++/ggmModel.cpp
@@ -4,7 +4,7 @@
   - Still need to check for double-connections before connecting
   - Implement update & sort CumDg
   - Start using HashNds
-  
+  - Improve BA model part (currently using vertex bag approach)
  */
 
 
@@ -25,7 +25,9 @@ int main(int argc, char *argv[]){
   int m0 = 5;                    // Initial number of nodes
   int NConns;                    // Number of connections
   
-  
+  //int NumberOfEdges=0;           //Total number of edges
+  std::vector<int>  nodeDegs(N, 0); //Vector with the degree of each node
+
   double *CumDg, *HashNds;
   CumDg   = (double *) malloc(N*sizeof(double));  // Cumulative sum
   HashNds = (double *) malloc(N*sizeof(double));  // Cumulative sum
@@ -52,7 +54,10 @@ int main(int argc, char *argv[]){
     for ( i = j+1; i < m0; i++ )
     {
       edge_list[i].push_back(j);
+      nodeDegs[i] += 1;
+
       edge_list[j].push_back(i);
+      nodeDegs[j] += 1;
     }
     
     // Initializing the cumulative vector
@@ -89,10 +94,12 @@ int main(int argc, char *argv[]){
 	}
 	
 	edge_list[to].push_back(from);
+	nodeDegs[to] += 1;
 	CumDg[to]++;
 		    
 	edge_list[from].push_back(to);
 	CumDg[from]++;
+	nodeDegs[from] += 1;
       }
       
     }
@@ -101,7 +108,47 @@ int main(int argc, char *argv[]){
       // Barabasi-Albert branch
       for ( k = 0; k < M; k++ )
       {
+	int degSum = 0; //Sum of degree
+	for(std::vector<int>::iterator it = nodeDegs.begin(); it != nodeDegs.end(); ++it)
+	  degSum += *it;
+
+	//std::cout << degSum << std::endl;
+
+	std::vector<int> vertexBag(degSum);
 	
+	int index = 0;
+
+	for(int position = 0; position < N; position++)
+	  for(int number = 0; number < nodeDegs[position]; number++)
+	    {
+	      vertexBag[index] = position;
+	      index += 1;
+	    }
+
+	bool isAValidConnection = false;
+	
+	while(!isAValidConnection)
+	  {
+	    int randPos = (degSum-1)*ureal(generator);
+	    to = vertexBag[randPos];
+
+	     if ( to != from && std::find(edge_list[to].begin(), edge_list[to].end(), from) == edge_list[to].end() )
+	       isAValidConnection = true;
+	  }
+	
+	//Free memory
+	vertexBag.clear();
+	vertexBag.shrink_to_fit();
+
+	edge_list[to].push_back(from);
+	nodeDegs[to] += 1;
+	CumDg[to]++;
+		    
+	edge_list[from].push_back(to);
+	CumDg[from]++;
+	nodeDegs[from] += 1;
+
+	/**
 	int Q = NConns*(ureal(generator));
 	i = 0;
 	while( i >= 0 )
@@ -114,12 +161,13 @@ int main(int argc, char *argv[]){
 	  
 	  i++;
 	}
+	**/
+
+	//edge_list[from].push_back(to);
+	//CumDg[from]++;
 	
-	edge_list[from].push_back(to);
-	CumDg[from]++;
-	
-	edge_list[to].push_back(from);
-	CumDg[to]++;
+	//edge_list[to].push_back(from);
+	//CumDg[to]++;
       }
       
     }
